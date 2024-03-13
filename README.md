4c4348477febb691098e6f5086083b7afabce1ba
Gyandraa <gyandranaupal14@gmail.com>
2024-03-13 23:53:04 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+Address Book Project Of Gya
+
+simple address book project with html css and javascript only.
---
+++ index.js
@@ -0,0 +1,22 @@
+const contacts = [
+    {
+        id: 1,
+        fullName: "M Haidar Hanif",
+        age: 30,
+        phone: "+621234567890",
+    },
+    {
+        id: 2,
+        fullName: "Elon Musk",
+        age: 50,
+        phone: "+1234567890",
+    },
+    {
+        id: 3,
+        fullName: "Jeff Bezos",
+        age: 52,
+        phone: "+1234567890",
+    },
+];
+
+console.log(contacts);
